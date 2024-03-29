fae324d00e4385ea33e0963ccfa9c5563aeb014a
Rajas Vyas <rajas.10689752@ltimindtree.com>
2024-03-29 18:12:45 +0530

    Creating a service & using its dependency injection in other components

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,18 +1,28 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, inject } from '@angular/core';
+import { LoggingService } from '../logging.service';
+
+//here we are using alternative way of dependecny injection apart form the providers 
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
-  styleUrls: ['./account.component.css']
+  styleUrls: ['./account.component.css'],
+  providers: [LoggingService]
 })
 export class AccountComponent {
   @Input() account: {name: string, status: string};
   @Input() id: number;
   @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
 
+  private logService?: LoggingService;
+
+  constructor() {
+    this.logService = inject(LoggingService);
+  }
 
   onSetTo(status: string) {
     this.statusChanged.emit({id: this.id, newStatus: status});
-    console.log('A server status changed, new status: ' + status);
+    this.logService.logStatusChange(status);
+    // console.log('A server status changed, new status: ' + status);
   }
 }
